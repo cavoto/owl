58ca8c977ec2e19b43b7c1634e0f9cb01c678a2a
Paulo Cavoto <pcavoto@br.ibm.com>
2015-11-07 16:29:38 -0200

    cleaning time!

--- public/js/graph.js
+++ public/js/graph.js
@@ -4,12 +4,7 @@ $(document).ready(function () {
 
     if (graphObj !== undefined) {
         if (graphObj.type === "pie") {
-            // Get context with jQuery - using jQuery's .get() method.
-            var ctx = $("#graph_pie").get(0).getContext("2d");
-            // This will get the first returned node in the jQuery collection.
-            var myNewChart = new Chart(ctx).Line(data);
 
-            //            var myLineChart = new Chart(ctx).Line(data, options);
         } else {
 
             // Get context with jQuery - using jQuery's .get() method.
@@ -23,7 +18,7 @@ $(document).ready(function () {
             });
 
             // This will get the first returned node in the jQuery collection.
-            var myNewChart = new Chart(ctx).Line(graphObj);
+            var myNewChart = new Chart(ctx).Line(graphObj,{});
 
         }
 
