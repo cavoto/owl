9b83dfafd540e09d59079a5ca74fc04235f731f5
Paulo Cavoto <pcavoto@br.ibm.com>
2015-11-11 15:41:10 -0200

    put a smile on you face

--- public/js/graph.js
+++ public/js/graph.js
@@ -17,8 +17,44 @@ $(document).ready(function () {
                 graphObj.datasets[key].fillColor = "rgba(220,220,220,0.2)";
             });
 
-            // This will get the first returned node in the jQuery collection.
-            var myNewChart = new Chart(ctx).Line(graphObj,{});
+            var options = {
+
+                    scaleFontFamily: "'FontAwesome'",
+                    // Boolean - If we should show the scale at all
+                    showScale: true,
+
+                    // Boolean - If we want to override with a hard coded scale
+                    scaleOverride: true,
+
+                    // ** Required if scaleOverride is true **
+                    // Number - The number of steps in a hard coded scale
+                    scaleSteps: 4,
+                    // Number - The value jump in the hard coded scale
+                    scaleStepWidth: 1,
+                    // Number - The scale starting value
+                    scaleStartValue: 0,
+                    // Boolean - Whether to show labels on the scale
+                    scaleShowLabels: true,
+   // Number - Pixel width of the scale line
+    scaleLineWidth: 2,
+
+    // Number - Scale label font size in pixels
+    scaleFontSize: 20,
+                    // Interpolated JS string - can access value
+                    scaleLabel: function (data) {
+
+                        var o = "";
+                        if (data.value == 1) {
+                            o = "\uf119"
+                        } else if (data.value == 2) {
+                            o = "\uf11a"
+                        } else if (data.value == 3) {o = "\uf118"}
+
+                        return o;
+                    }
+                }
+                // This will get the first returned node in the jQuery collection.
+            var myNewChart = new Chart(ctx).Line(graphObj, options);
 
         }
 
