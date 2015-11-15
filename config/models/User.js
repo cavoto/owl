272e339c240636c103ed7a8f478a274785253746
Paulo Cavoto <pcavoto@br.ibm.com>
2015-11-15 18:17:29 -0200

    another attempt of a new user card

--- config/models/User.js
+++ config/models/User.js
@@ -13,21 +13,6 @@ var userSchema = new Schema({
     sentiment_sum: { type: Number, default: 0 }
 });
 
-//callSchema.pre("save", function(next) {
-//    var self = this;
-//
-//    mongoose.models["Call"].findOne({username : self.user},function(err, user) {
-//        console.log("e ai ?");
-//        if(user) {
-//            console.log("achei");
-//        } else {
-//            console.log("nao achei");
-//        }
-//        }
-//    });
-//    next();
-//});
-
 var User = mongoose.model('User', userSchema);
 
 module.exports = User;
